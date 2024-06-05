b50bb99f54926c707fe6e40f53935b77f01cad44
Ameen962 <ameenkamal71@gmail.com>
2024-06-05 18:03:02 +0300

    track

--- src/gpx1/tracks.py
+++ src/gpx1/tracks.py
@@ -1,130 +1,121 @@
-"""
-tracks.py
-
+""" track.py
 
 
 Author: Amin Kamal
 
 """
 
-import gpxpy
 import gpxpy.gpx
+import lxml
 
-def _get_tracks(input_gpx: gpxpy.gpx.GPX) -> list:
-    """Creates a list of all track points with their corresponding latitude, longitude, and optional elevation
+def _get_trkpts(input_gpx: gpxpy.gpx.GPX) -> list:
+    """Creates a list of all trackpoints with their associated latitude, longitude, and optional elevation
 
     Args:
-        input_gpx (gpxpy.gpx.GPX): Data from the GPX file
+        input_gpx (gpxpy.gpx.GPX): GPX file data
 
     Returns:
         list: List with latitude, longitude, elevation
     """
     
-    tracks = []
+    trkpts = []
     
-    for track in input_gpx.tracks:
-        for segment in track.segments:
-            for point in segment.points:
-                lat = point.latitude
-                lon = point.longitude
-                ele = point.elevation if point.elevation is not None else ""
-                tracks.append([lat, lon, ele])
+    # Search for all trackpoints in the GPX file
+    for trk in input_gpx.tracks:
+        for trkseg in trk.segments:
+            for trkpt in trkseg.points:
+                lat = trkpt.latitude
+                lon = trkpt.longitude
+                ele = trkpt.elevation if trkpt.elevation is not None else ""
+                trkpts.append([lat, lon, ele])
     
-    return tracks
+    return trkpts
 
 def print_list(input_gpx: gpxpy.gpx.GPX) -> None:
-    """Prints a list of all track points with their corresponding latitude, longitude, and optional elevation
+    """Prints a list of all trackpoints with their associated latitude, longitude, and optional elevation
 
     Args:
-        input_gpx (gpxpy.gpx.GPX): Data from the GPX file
+        input_gpx (gpxpy.gpx.GPX): GPX file data
     """
     
     print("   ID   |  Latitude  |  Longitude  |  Elevation")
     print("--------|------------|-------------|-------------")
     
-    tracks = _get_tracks(input_gpx)
+    # Create a list of all trackpoints
+    trkpts = _get_trkpts(input_gpx)
     
-    for id, track in enumerate(tracks):
-        if track[2] != "":
-            print(f"  {id:04}  |  {track[0]:9.6f} |  {track[1]:9.6f}  | {track[2]:9.6f}")
+    # Print the trackpoint information in list form
+    for id, trkpt in enumerate(trkpts):
+        if trkpt[2] != "":
+            print(f"  {id:04}  |  {trkpt[0]:9.6f} |  {trkpt[1]:9.6f}  | {trkpt[2]:9.6f}")
         else:
-            print(f"  {id:04}  |  {track[0]:9.6f} |  {track[1]:9.6f}  | {track[2]} ")
+            print(f"  {id:04}  |  {trkpt[0]:9.6f} |  {trkpt[1]:9.6f}  | {trkpt[2]}")
 
 def get_count(input_gpx: gpxpy.gpx.GPX) -> int:
-    """Returns the number of track points in the file
+    """Returns the number of trackpoints in the file
 
     Args:
-        input_gpx (gpxpy.gpx.GPX): Data from the GPX file
+        input_gpx (gpxpy.gpx.GPX): GPX file data
 
     Returns:
-        int: Number of track points
+        int: Number of trackpoints
     """
     
-    tracks = _get_tracks(input_gpx)
+    trkpts = _get_trkpts(input_gpx)
     
-    return len(tracks)
+    return len(trkpts)
 
 def calc_elevation(id1: int, id2: int, input_gpx: gpxpy.gpx.GPX) -> float:
-    """Returns the elevation difference between two track points
+    """Returns the elevation difference between two trackpoints
 
     Args:
-        id1 (int): ID of the first track point
-        id2 (int): ID of the second track point
-        input_gpx (gpxpy.gpx.GPX): Data from the GPX file
+        id1 (int): ID of the first trackpoint
+        id2 (int): ID of the second trackpoint
 
     Returns:
         float: Elevation difference
     """
     
-    tracks = _get_tracks(input_gpx)
+    trkpts = _get_trkpts(input_gpx)
     
-    if not (0 <= id1 < len(tracks)):
-        print("Error 200: Track point 1 not found!")
-        return
+    if not (0 <= id1 < len(trkpts)):
+        print("Error 200: Trackpoint 1 not found!")
+        return None
     
-    if not (0 <= id2 < len(tracks)):
-        print("Error 201: Track point 2 not found!")
-        return
+    if not (0 <= id2 < len(trkpts)):
+        print("Error 201: Trackpoint 2 not found!")
+        return None
     
-    if tracks[id1][2] == "":
-        print("Error 202: No elevation information for track point 1!")
-        return
+    if trkpts[id1][2] == "":
+        print("Error 202: No elevation information for Trackpoint 1!")
+        return None
     
-    if tracks[id2][2] == "":
-        print("Error 203: No elevation information for track point 2!")
-        return
+    if trkpts[id2][2] == "":
+        print("Error 203: No elevation information for Trackpoint 2!")
+        return None
     
-    ele_diff = float(tracks[id2][2]) - float(tracks[id1][2])
+    ele_diff = float(trkpts[id2][2]) - float(trkpts[id1][2])
     print(f"Elevation difference = {ele_diff}")
-    
     return ele_diff
 
 def edit(id: int, lat: float, lon: float, ele: float, input_gpx: gpxpy.gpx.GPX) -> gpxpy.gpx.GPX:
-    """Changes the latitude, longitude, and elevation of a given track point
+    """Changes the latitude, longitude, and elevation of a given trackpoint
 
     Args:
-        id (int): ID of the track point to edit
+        id (int): ID of the trackpoint to edit
         lat (float): Latitude
         lon (float): Longitude
         ele (float): Elevation
-        input_gpx (gpxpy.gpx.GPX): Data from the GPX file
+        input_gpx (gpxpy.gpx.GPX): GPX file data
 
     Returns:
         gpxpy.gpx.GPX: Edited GPX data
     """
     
-    track_points = []
-    for track in input_gpx.tracks:
-        for segment in track.segments:
-            track_points.extend(segment.points)
-    
-    if not (0 <= id < len(track_points)):
-        print("Error 204: Track point not found!")
-        return
-    
-    track_points[id].latitude = lat
-    track_points[id].longitude = lon
-    track_points[id].elevation = ele
+    trackpoints = list(input_gpx.walk())
+    trackpoints[id].latitude = lat
+    trackpoints[id].longitude = lon
+    trackpoints[id].elevation = ele
     
     print_list(input_gpx)
     return input_gpx
