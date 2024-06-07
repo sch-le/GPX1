06ec5677f04a2a0052246b0049fcd322fc6fd8cd
sch-le <142419992+sch-le@users.noreply.github.com>
2024-06-07 17:48:30 +0200

    ...

--- src/gpx1/file_management.py
+++ src/gpx1/file_management.py
@@ -11,7 +11,7 @@ import os
 #Importieren der notwendigen Funktionen aus parser.py
 from gpx1.parser import parse, write_file
 
-def open_file():                                                 
+def open_file():                                              
     filepath = filedialog.askopenfilename(
         #Beschränken der Auswahl auf GPX-Dateien
         filetypes=[("GPX files", "*.gpx")]  
@@ -21,7 +21,7 @@ def open_file():
         if not filepath.endswith('.gpx'):
             print("Fehler: Bitte wählen Sie eine GPX-Datei aus.")
         else:
-            try:
+            #try:
                 #Verwenden der parse-Funktion aus parser.py
                 gpx_data = parse(filepath)  
                 if gpx_data:
@@ -29,20 +29,21 @@ def open_file():
                     write_file(gpx_data)  #GPX-Datei schreiben
                 else:
                     print("Fehler beim Parsen der GPX-Datei.")
-            except Exception as e:
-                print(f"Fehler beim Laden der Datei: {e}")
+            #except Exception as e:
+            #    print(f"Fehler beim Laden der Datei: {e}")
     else:                                                                   
         print("Keine Datei ausgewählt.")                                    
 
-    window.destroy()
+    #window.destroy()
 
-#Erstellen des Hauptfensters
-window = Tk()                                                               
-window.title("GPX Datei Öffnen")
+def open_window():
+    #Erstellen des Hauptfensters
+    window = Tk()                                                               
+    window.title("GPX Datei Öffnen")
 
-#Erstellen und platzieren des "Öffnen" Buttons
-button = Button(window, text="Öffnen", command=open_file)
-button.pack(pady=20)
+    #Erstellen und platzieren des "Öffnen" Buttons
+    button = Button(window, text="Öffnen", command=open_file)
+    button.pack(pady=20)
 
-#Starten der Tkinter-Ereignisschleife
-window.mainloop()
+    #Starten der Tkinter-Ereignisschleife
+    window.mainloop()
