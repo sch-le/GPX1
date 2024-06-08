7f7b8c9edb80a6f6ab84d8d1d06ba10529bdeb12
sch-le <142419992+sch-le@users.noreply.github.com>
2024-06-08 13:39:59 +0200

    unnötige dateien gelöscht

--- src/gpx1/test222.py
+++
@@ -1,6 +0,0 @@
-# C:\Users\user\Desktop\GPX1-main\src\gpx1\test_import.py
-try:
-    from gpx1 import track_module
-    print("Module imported successfully.")
-except ImportError as e:
-    print(f"Error importing module: {e}")
